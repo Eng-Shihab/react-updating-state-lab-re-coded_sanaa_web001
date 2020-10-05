79518e0416daeed1e801e8e605a89a222dba69f1
Shihab AL-Mekhlafi <shihabalmekhlafy@gmail.com>
2020-10-05 23:34:24 +0000

    Automatically backed up by Learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,10 +1,8 @@
 // Code DigitalClicker Component Here
 import React from 'react';
 
-export default class DigitalClicker extends React.Component {
-
-
-   constructor() {
+class DigitalClicker extends React.Component {
+  constructor() {
     super();
 
     this.state = {
@@ -12,45 +10,17 @@ export default class DigitalClicker extends React.Component {
     };
   }
 
-  onClick = () => {
-    this.setState(prevState => {
-        return {
+  handleClick = () => {
+    this.setState(prevState => ({
       timesClicked: prevState.timesClicked+1
-    }
-    })
+    }))
   }
 
   render() {
     return (
-      <button onClick={this.onClick}>{this.state.timesClicked}</button>
+      <button onClick={this.handleClick}>{this.state.timesClicked}</button>
     );
   }
 }
-// Code DigitalClicker Component Here
-import React from 'react';
 
-export default class DigitalClicker extends React.Component {
-
-
-   constructor() {
-    super();
-
-    this.state = {
-      timesClicked: 0,
-    };
-  }
-
-  onClick = () => {
-    this.setState(prevState => {
-        return {
-      timesClicked: prevState.timesClicked+1
-    }
-    })
-  }
-
-  render() {
-    return (
-      <button onClick={this.onClick}>{this.state.timesClicked}</button>
-    );
-  }
-}
+export default DigitalClicker;
